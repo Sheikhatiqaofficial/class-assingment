388a2cdd638f9bdbb47f2af384931eddaf258bb0
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2025-02-20 22:14:37 +0500

    commit

---
+++ React-app/src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <>
+
+    <h1 class = "app">React App</h1>
+<p class ="para">We got to know about React, which is libary of Java script, in our recent web development class, and today we created the simplest React app by installing it, removing unnecessary files and folders, and adding some styling to the root page. Things are getting more interesting day by day!
+</p>
+<div class="box">
+<div class ="box1"></div>
+<div class ="box2"></div>
+<div class ="box3"></div>
+</div>
+  </>
+  );
+}
+
+export default App;
---
+++ Vite-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import "./styles.css";
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 class = "app">Vite App</h1>
+<p class ="para">We got to know about Vite in our recent web development class, and today we created the simplest Vite app by installing it, removing unnecessary files and folders, and adding some styling to the root page. Things are getting more interesting day by day!
+</p>
+<div class="box">
+<div class ="box1"></div>
+<div class ="box2"></div>
+<div class ="box3"></div>
+</div>
+    </>
+  )
+}
+
+export default App
